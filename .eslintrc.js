805fc8c9369865d3cf92193334f6ba9eae915095
tsuyoshiwada <v23.b.wada@gmail.com>
2017-08-04 02:03:08 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+/* eslint-disable quote-props */
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+  },
+  'parser': 'babel-eslint',
+  'extends': 'airbnb',
+  'rules': {
+    'react/jsx-filename-extension': 'off',
+    'react/no-find-dom-node': 'off',
+  },
+};
